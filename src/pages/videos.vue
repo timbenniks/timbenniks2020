614479ecd2e41e73e72c8efb3ca171255fa10ebf
Tim Benniks <tbenniks@gmail.com>
2020-04-24 17:23:52 +0200

    Iniotial grid for videos added

--- src/pages/videos.vue
+++ src/pages/videos.vue
@@ -10,11 +10,40 @@
           :title="$page.Prismic.videos.title"
         />
 
-        <div class="posts">
+        <div class="posts videos">
           <article
             v-for="post in $page.Prismic.allVideos.edges"
             :key="post.node._meta.uid"
-            class="post"
+            class="post video"
+          >
+            <lazy-image
+              ratio="16/9"
+              :alt="$prismic.asText(post.node.title)"
+              :url="post.node.image.url"
+              :caption="false"
+              :widths="[300, 400, 500, 600, 680]"
+              sizes="(max-width: 400px) 100vw, (min-width: 700px) 210px"
+            />
+
+            <div class="post-content-wrap">
+              <p class="post-title">
+                <g-link :to="`/videos/${post.node._meta.uid}/`">
+                  {{ $prismic.asText(post.node.title) }}
+                </g-link>
+              </p>
+              <p class="post-description">
+                <span class="post-date">
+                  {{ $prismic.asDay(post.node.publication_date) }}
+                  {{ $prismic.asMonth(post.node.publication_date) }}
+                  {{ $prismic.asYear(post.node.publication_date) }}
+                </span>
+              </p>
+            </div>
+          </article>
+          <article
+            v-for="post in $page.Prismic.allVideos.edges"
+            :key="post.node._meta.uid"
+            class="post video"
           >
             <lazy-image
               ratio="16/9"
@@ -57,7 +86,6 @@ query {
             tags
           }
           title
-          video_embed
           publication_date
           image
         }
@@ -109,6 +137,33 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.videos {
+  display: grid;
+  grid-gap: 1rem;
+
+  @include responsive(
+    'grid-template-columns',
+    (
+      xs: 100%,
+      s: 50% 50%,
+      m: 33% 33% 33%,
+      l: 25% 25% 25% 25%,
+    )
+  );
+
+  .video {
+    width: auto;
+    margin: 0;
+
+    figure {
+      float: none;
+      width: 100%;
+      height: auto;
+      margin: rem(0 0 16px 0);
+    }
+  }
+}
+
 .post-date {
   font-weight: 400;
   float: none;
