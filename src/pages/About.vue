295964887c4e93be8471f94c1dacf8f867bdd885
Tim Benniks <tbenniks@gmail.com>
2020-06-15 20:34:13 +0200

    added query

---
+++ src/pages/About.vue
@@ -0,0 +1,55 @@
+<template>
+  <Layout>
+    <div class="content-wrapper about">
+      <navigation />
+
+      <main id="main-content">
+        {{ $page.about }}
+      </main>
+    </div>
+  </Layout>
+</template>
+
+<page-query>
+query {
+  about: prismicSinglePage(id:"about-tim-benniks") {
+    uid
+    slug
+    data {
+      title
+      sub_title
+      content
+      social_cards {
+        type
+        content {
+          title
+          description
+          image
+        }
+      }
+    }
+  }
+}
+</page-query>
+
+<script>
+import Navigation from '../components/navigation.vue'
+import FancyTitle from '../components/fancy-title.vue'
+import LazyImage from '../components/lazy-image.vue'
+import LinkMixin from '../mixins/linkMixin'
+import mapMetaInfo from '../prismic/mapMetaInfo'
+
+export default {
+  components: {
+    Navigation,
+    LazyImage,
+    FancyTitle,
+  },
+  mixins: [LinkMixin],
+  metaInfo() {
+    return mapMetaInfo(this.$page.home.data, 'home', this.$router.currentRoute)
+  },
+}
+</script>
+
+<style lang="scss"></style>
